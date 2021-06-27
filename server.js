920b1762508294e48340368d4729b6b644245f8b
unknown <nick_clymer2018@yahoo.com>
2021-06-26 21:48:31 -0600

    added mongodb uri access

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ var mongoose = require("mongoose");
 
 var app = express();
 var PORT = process.env.PORT || 3002;
+const uri = process.env.MONGODB_URI;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(require("./routes/apiRoutes"))
 app.use(require("./routes/viewRoutes"))
-
-// require("./routes/apiRoutes")(app);
-// require("./routes/viewRoutes")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
